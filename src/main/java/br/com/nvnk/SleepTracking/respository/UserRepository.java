b46129abdf203498f81ea80ac8c50ca302fe2789
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-09-15 20:18:28 -0300

    feat: add UserRepository with MongoDB integration

---
+++ src/main/java/br/com/nvnk/SleepTracking/respository/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.nvnk.SleepTracking.respository;
+
+import br.com.nvnk.SleepTracking.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail (String Email);
+}
