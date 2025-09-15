c4c718cf089d7ef7b1fee36ddd5735307ec3fa6f
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-09-15 20:02:14 -0300

    chore: Initial commit

---
+++ src/main/java/br/com/nvnk/SleepTracking/SleepTrackingApplication.java
@@ -0,0 +1,13 @@
+package br.com.nvnk.SleepTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepTrackingApplication.class, args);
+	}
+
+}
