5cde9f71cf1b8c6cb4f69e2aebaa4f335b9991a1
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-09-18 17:39:11 -0300

    feat: add functionality to describe user

---
+++ src/main/java/br/com/nvnk/SleepTracking/controller/UserController.java
@@ -0,0 +1,27 @@
+package br.com.nvnk.SleepTracking.controller;
+
+import br.com.nvnk.SleepTracking.mapper.UserMapper;
+import br.com.nvnk.SleepTracking.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService service;
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> describeUser (){
+        Map<String, Object> response = new HashMap<>();
+        response.put("User: ", UserMapper.toResponse(service.describeUser()));
+
+        return ResponseEntity.ok(response);
+    }
+}
--- src/main/java/br/com/nvnk/SleepTracking/service/UserService.java
+++ src/main/java/br/com/nvnk/SleepTracking/service/UserService.java
@@ -1,10 +1,12 @@
 package br.com.nvnk.SleepTracking.service;
 
 import br.com.nvnk.SleepTracking.entity.User;
-import br.com.nvnk.SleepTracking.exception.UserEmailAlreadyInUse;
+import br.com.nvnk.SleepTracking.exception.UserEmailAlreadyInUseException;
 import br.com.nvnk.SleepTracking.exception.UserNotFoundException;
 import br.com.nvnk.SleepTracking.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +19,7 @@ public class UserService {
 
     public User save (User user){
         if (repository.findByEmail(user.getEmail()).isPresent()){
-            throw new UserEmailAlreadyInUse(("This email address is already in use."));
+            throw new UserEmailAlreadyInUseException(("This email address is already in use."));
         }
 
         String password = user.getPassword();
@@ -26,11 +28,19 @@ public class UserService {
     }
 
     public User findByEmail (String email){
-        return repository.findByEmail(email).orElseThrow(()-> new UserNotFoundException("Username not found."));
+        return repository.findByEmail(email).orElseThrow(()-> new UserNotFoundException("User not found."));
     }
 
     public User findById (String id){
-        return repository.findById(id).orElseThrow(()-> new UserNotFoundException("Username not found."));
+        return repository.findById(id).orElseThrow(()-> new UserNotFoundException("User not found."));
+    }
+
+    public User describeUser (){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        String userId = user.getId();
+
+        return repository.findById(userId).orElseThrow(()-> new UserNotFoundException("User not found."));
     }
 
 }
