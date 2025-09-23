4f27d9644929c7f0fbf75067c8438f5fff974f29
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-09-23 19:19:47 -0300

    feat: add a method to find sleep attempts according to a specific ID and period

--- src/main/java/br/com/nvnk/SleepTracking/repository/SleepAttemptRepository.java
+++ src/main/java/br/com/nvnk/SleepTracking/repository/SleepAttemptRepository.java
@@ -4,6 +4,7 @@ import br.com.nvnk.SleepTracking.entity.SleepAttempt;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +12,5 @@ import java.util.Optional;
 public interface SleepAttemptRepository extends MongoRepository<SleepAttempt, String> {
     Optional<List<SleepAttempt>> findByUserId (String userId);
     Optional<List<SleepAttempt>> findByUserIdAndSuccess(String userId, boolean success);
+    Optional<List<SleepAttempt>> findByUserIdAndBedTimeBetween(String userId, LocalDateTime startOfDay, LocalDateTime endOfDay);
 }
