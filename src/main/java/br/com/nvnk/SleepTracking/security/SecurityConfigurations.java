4ec7bc1cc6851dc9dae9faecbd5b7758e36dc674
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-09-25 22:58:16 -0300

    feat: add admin permissions

--- src/main/java/br/com/nvnk/SleepTracking/security/SecurityConfigurations.java
+++ src/main/java/br/com/nvnk/SleepTracking/security/SecurityConfigurations.java
@@ -27,12 +27,14 @@ public class SecurityConfigurations {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return  httpSecurity
+        return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**", "/sleep/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
